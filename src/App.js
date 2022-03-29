8bd37b0281ea202194f2a71390e00f46e0b2d782
gabriela729 <99573989+gabriela729@users.noreply.github.com>
2022-03-29 11:26:44 -0500

    interfaz con React

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    
+    <div className="container">
+      <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <h1 className="title">
+    <img src="https://static.vecteezy.com/system/resources/previews/000/610/399/non_2x/vector-relationship-people-logos-school-education-logo-design-concept.jpg"/>
+    <br></br>
+    <br></br>
+    
+    <br></br>
+       
+    </h1>
+    
+    <h1 className="title">
+      
+    </h1>
+    
+    
+    
+ </div>
+  );
+}
+
+export default App;
